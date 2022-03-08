77b271489f3eee0a8e41908143577131151557bf
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-08 15:03:15 -0500

    Adding Sass Files

--- public/javascripts/wedding.js
+++ public/javascripts/wedding.js
@@ -1,1 +1,7 @@
 import '../sass/style.scss';
+
+import {$, $$} from './modules/bling'
+import makeMap from './modules/map';
+import autocomplete from './modules/autocomplete';
+
+makeMap( $('#map'));
